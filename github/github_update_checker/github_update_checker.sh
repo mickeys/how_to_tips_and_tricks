724ac91ccc5d1b226446ab347be875e26cd72813
Michael Sattler <michael.sattler@creditkarma.com>
2019-05-22 07:06:44 -0700

    returns booleans

--- github/github_update_checker/github_update_checker.sh
+++ github/github_update_checker/github_update_checker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# TO-DO: return '' if up-to-date, $remote instead, so the user can store it
+# via mktemp() and offer it to the caller, or swap, ...
+
 # -----------------------------------------------------------------------------
 # ghUpdateCheck -- Github update checker()
 #
@@ -24,19 +27,37 @@ ghUpdateCheck () {
 	local FN="$7"						# filename to be checked
 	local BRANCH="$8"					# 'master' or some other branch
 
+	l_sha=$( shasum < "README.md" ) #"${BASH_SOURCE[0]}" )
+	l_sha="${l_sha%% *}"				# get the first token, the hash
+echo "l_sha: \"${l_sha}\""
+
+FN="README.md"
 	# -------------------------------------------------------------------------
 	# Note: the 'accept' header is sent to future-proof this code for when the
 	# default API level changes; see developer.github.com/v3/
 	# -------------------------------------------------------------------------
-	local=$( shasum < "${BASH_SOURCE[0]}" )
+# remote=$( curl --user "${LOGIN}:${PW_OR_KEY}" \ --header 'Accept: application/vnd.github.v3+json' \ "https://${GH}/${OWNER}/${REPO}/contents/${FP}/${FN}?ref=${BRANCH}" 2>/dev/null \ | jq --raw-output '.content' | base64 -D | shasum )
+
+#	remote=$( curl -u "${LOGIN}:${PW_OR_KEY}" \
+#		--header 'Accept: application/vnd.github.v3+json' \
+#		"https://${GH}/${OWNER}/${REPO}/contents/${FP}/${FN}?ref=${BRANCH}" 2>/dev/null \
+#		| jq --raw-output '.content' | base64 -D | shasum )
+
+LOGIN='mickeys'
+PW_OR_KEY='9d05ff129ae6a1bda4a2081f6e94d85c82179ffe'
 
 	remote=$( curl -u "${LOGIN}:${PW_OR_KEY}" \
 		--header 'Accept: application/vnd.github.v3+json' \
 		"https://${GH}/${OWNER}/${REPO}/contents/${FP}/${FN}?ref=${BRANCH}" 2>/dev/null \
-		| jq --raw-output '.content' | base64 -D | shasum )
+		| jq --raw-output '.content' | base64 -D )
+	r_sha=$( echo -n "$remote" | shasum )
+	r_sha="${r_sha%% *}"				# get the first token, the hash
+echo "r_sha: \"${r_sha}\""
 
-	[[ "${local%% *}" != "${remote%% *}" ]]	# 0 = is latest | 1 = update needed
+	[[ "${l_sha}" != "${r_sha}" ]]		# 0 = is latest | 1 = update needed
 }
+
+#TO-DO: capture the downloaded content and either offer to update and swap this->this.old and temp->this or something
 
 # -----------------------------------------------------------------------------
 # Example of ghUpdateCheck() being called and return code analyzed.
@@ -48,6 +69,9 @@ if ghUpdateCheck "${OWNER}" "${GH_AKEY}" \
 	'mickeys' \
 	'how_to_tips_and_tricks' 'github/github_update_checker' \
 	"${thisFilename}" 'master' ; then
+		# ---------------------------------------------------------------------
+		#
+		# ---------------------------------------------------------------------
 		echo "There's a later version of ${thisFilename} available."
 	else
 		echo "You're running the latest version of ${thisFilename}."
