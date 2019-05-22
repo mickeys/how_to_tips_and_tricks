0184adce1d0ffcf50104b7221a741da81130a97a
Michael Sattler <michael.sattler@creditkarma.com>
2019-05-22 08:38:28 -0700

    hand back latest commit contents

--- github/github_update_checker/github_update_checker.sh
+++ github/github_update_checker/github_update_checker.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# TO-DO: return '' if up-to-date, $remote instead, so the user can store it
-# via mktemp() and offer it to the caller, or swap, ...
-
 # -----------------------------------------------------------------------------
 # ghUpdateCheck -- Github update checker()
 #
@@ -15,7 +12,9 @@
 # the two differ the assumption is made that the reason is a subsequent commit
 # has happened and the user should update the local copy.
 #
-# Return is 0 success (up-to-date) or 1 failure (local file needs updating).
+# Return value is:
+#	success - file is up-to-date - empty string
+#	failure - file is out of sync - contents of the latest commit
 # -----------------------------------------------------------------------------
 ghUpdateCheck () {
 	local LOGIN="$1"					# 'username'
@@ -29,7 +28,6 @@ ghUpdateCheck () {
 
 	l_sha=$( shasum < "README.md" ) #"${BASH_SOURCE[0]}" )
 	l_sha="${l_sha%% *}"				# get the first token, the hash
-echo "l_sha: \"${l_sha}\""
 
 FN="README.md"
 	# -------------------------------------------------------------------------
@@ -46,32 +44,39 @@ FN="README.md"
 LOGIN='mickeys'
 PW_OR_KEY='9d05ff129ae6a1bda4a2081f6e94d85c82179ffe'
 
-	remote=$( curl -u "${LOGIN}:${PW_OR_KEY}" \
+	r=$( curl -u "${LOGIN}:${PW_OR_KEY}" \
 		--header 'Accept: application/vnd.github.v3+json' \
 		"https://${GH}/${OWNER}/${REPO}/contents/${FP}/${FN}?ref=${BRANCH}" 2>/dev/null \
 		| jq --raw-output '.content' | base64 -D )
-	r_sha=$( echo -n "$remote" | shasum )
-	r_sha="${r_sha%% *}"				# get the first token, the hash
-echo "r_sha: \"${r_sha}\""
 
-	[[ "${l_sha}" != "${r_sha}" ]]		# 0 = is latest | 1 = update needed
+	r_sha=$( echo -n "$r" | shasum )	# calculate remote checksum
+	r_sha="${r_sha%% *}"				# get the first token, the hash
+
+	if [[ "${l_sha}" != "${r_sha}" ]] ; then
+		echo "${r}"				# here's the latest commit's contents
+	else
+		echo ''							# all is good; nothing for you to do
+	fi
+	return 0							# numeric return code ignored
 }
 
-#TO-DO: capture the downloaded content and either offer to update and swap this->this.old and temp->this or something
-
 # -----------------------------------------------------------------------------
-# Example of ghUpdateCheck() being called and return code analyzed.
+# Example of ghUpdateCheck() being called and handling of the result.
 # -----------------------------------------------------------------------------
 thisFilename="$(basename ${BASH_SOURCE[0]})"
 
-if ghUpdateCheck "${OWNER}" "${GH_AKEY}" \
+update=$(ghUpdateCheck "${OWNER}" "${GH_AKEY}" \
 	'api.github.com/repos' \
 	'mickeys' \
 	'how_to_tips_and_tricks' 'github/github_update_checker' \
-	"${thisFilename}" 'master' ; then
+	"${thisFilename}" 'master' )
 		# ---------------------------------------------------------------------
 		#
 		# ---------------------------------------------------------------------
+	if [ -n "${update}" ] ; then
+		tempd=$(mktemp -d) || { echo "Temporary file creation failed."; exit 1; }
+		tempf="${tempd}/$(date +%Y%m%d_%H%M%S)_${thisFilename}"
+		echo "${update}" >| "${tempf}"
 		echo "There's a later version of ${thisFilename} available."
 	else
 		echo "You're running the latest version of ${thisFilename}."
